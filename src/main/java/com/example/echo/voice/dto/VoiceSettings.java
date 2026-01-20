3a04ea79c1cba2e971760df7c9fc2129331d4d16
이형석 <2hyeongseok@tukorea.ac.kr>
2026-01-21 00:38:46 +0900

    docs: dto-SttResponse,VoiceSettings,WhisperTranscriptionResponse에 설명 주석 추가

--- src/main/java/com/example/echo/voice/dto/VoiceSettings.java
+++ src/main/java/com/example/echo/voice/dto/VoiceSettings.java
@@ -1,4 +1,7 @@
-﻿package com.example.echo.voice.dto;
+﻿/*
+음성 설정 정보
+*/
+package com.example.echo.voice.dto;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -6,6 +9,6 @@ import lombok.Getter;
 @Getter
 @Builder
 public class VoiceSettings {
-    private Double speed;
-    private String voiceName;
+    private Double speed;  //음성 속도
+    private String voiceName; // 음성 종류
 }
